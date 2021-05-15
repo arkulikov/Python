6f4f208523187653d7f5674853b7ee6e05b5b5d1
Artem <arkulikov@yandex.ru>
2021-05-15 10:25:16 +0300

    task 5. Correct solution

--- task_5.py
+++ task_5.py
@@ -11,23 +11,28 @@
 user_number = int(input("Введите элемент рейтинга (число больше нуля): "))
 my_list = [7, 5, 3, 3, 2]
 
-count_all_el = my_list.count(user_number) # кол-во чисел введенных юзером в списке
+count_all_el = my_list.count(user_number)  # кол-во чисел введенных юзером в списке
 
-if count_all_el > 0:
-    index_first_el = my_list.index(user_number)
-    my_list.insert(index_first_el + count_all_el, user_number)
-elif count_all_el == 0:
-    i = 1
-    for el in range(int(len(my_list))):
-            if user_number > my_list[i - 1]:
-                my_list.insert(i - 1, user_number)
+if user_number < 0 or user_number == 0:
+    print("Введите число больше нуля!")
+else:
+    if count_all_el > 0:
+        index_first_el = my_list.index(user_number)
+        my_list.insert(index_first_el + count_all_el, user_number)
+    elif count_all_el == 0:
+        i = 0
+        for el in range(int(len(my_list))):
+            if user_number > my_list[i]:
+                my_list.insert(i, user_number)
                 break
-            else:
-                if user_number > my_list[i - 1]:
-                    my_list.insert(i - 1, user_number)
-                    break
+            elif user_number < my_list[i]:
+                if user_number > my_list[i +1]:
+                    my_list.insert(i + 1, user_number)
                 else:
-                    i += 1
-                    continue
+                    if my_list[i + 1] == my_list[-1]:
+                        my_list.append(user_number)
+                    else:
+                        i += 1
+                        continue
 
-print(my_list)
+    print(my_list)
