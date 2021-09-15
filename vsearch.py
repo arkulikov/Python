bd1809b289d3e2e2770ffa4dc817341d74924f66
Artem <arkulikov@yandex.ru>
2021-09-15 21:09:06 +0300

    added argument to function 'searchletters()'

--- vsearch.py
+++ vsearch.py
@@ -3,7 +3,8 @@ def search4vowels(phrase:str) -> set:
     vowels = set('aeiou')
     return vowels.intersection(set(phrase))
 
-def search4letters(phrase:str, letters:str) -> set:
+def search4letters(phrase:str, letters:str='aeiou') -> set:
     """Возвращает множество букв из 'letters', найденных в указанной фразе"""
     return set(letters).intersection(set(phrase))
+
 
